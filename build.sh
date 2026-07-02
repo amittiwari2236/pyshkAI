a9df840030c8413d9c727e5eae521358d3493f6b
unknown <amittiwari.scholarhat@gmail.com>
2026-07-02 19:36:07 +0530

    Fix superuser creation username

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ python manage.py collectstatic --no-input
 python manage.py migrate
 
 # Automatically create a default superuser (since Render Free tier has no Shell)
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='amittiwari2236').exists() or User.objects.create_superuser('amittiwari2236', 'amittiwari2236@example.com', 'admin123')"
